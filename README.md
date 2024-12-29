f7f57f677f292b4cb1af6b202b96a95f051f3ef0
缪心雨 <1286280961@qq.com>
2024-12-29 18:42:10 +0800

    练习4：使用 LocalDate 和 LocalTime 处理日期和时间

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
   - 练习1：多模块项目
   - 练习2：利用 Records 创建数据传输对象（DTO）
   - 练习3：结合 Switch Expressions 实现高级路由逻辑
+  - 练习4：使用 LocalDate 和 LocalTime 处理日期和时间
---
+++ spring-boot-quickstart/src/main/java/top/xym/springboot/quickstart/service/MeetingService.java
@@ -0,0 +1,30 @@
+package top.xym.springboot.quickstart.service;
+
+import org.springframework.stereotype.Service;
+import top.xym.springboot.quickstart.entity.Meeting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MeetingService {
+
+    private final List<Meeting> meetings = new ArrayList<>();
+
+    /**
+     * 检查会议室是否可用于新的会议 （newMeeting）
+     *
+     * @param newMeeting 要检查的新会议对象
+     * @return 如果会议室在新会议的时间段内可用，则返回 true; 否则返回 false
+     */
+    public boolean isRoomAvailable(Meeting newMeeting) {
+        // 使用 Java Stream API 来检查会议列表中是否有新会议重叠的会议
+        return meetings.stream()
+                // 如果存在任何与新会议时间重叠的现有会议，返回 false
+                .noneMatch(existingMeeting -> existingMeeting.isOverlapping(newMeeting));
+    }
+
+    public void addMeeting(Meeting meeting) {
+        meetings.add(meeting);
+    }
+}
