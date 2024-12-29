2ca4b06be7707dc7f0ec27f30cc9f755cf9153a8
缪心雨 <1286280961@qq.com>
2024-12-29 19:39:07 +0800

    练习5：使用 Stream API 实现简单的用户过滤功能

---
+++ spring-boot-quickstart/src/main/java/top/xym/springboot/quickstart/controller/UserController.java
@@ -0,0 +1,25 @@
+package top.xym.springboot.quickstart.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.xym.springboot.quickstart.service.UserService;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/adults")
+    public List<String> getAdultUserNames() {
+        return userService.getAdultUserNames();
+    }
+}
---
+++ spring-boot-quickstart/src/main/java/top/xym/springboot/quickstart/entity/User.java
@@ -0,0 +1,25 @@
+package top.xym.springboot.quickstart.entity;
+
+public class User {
+    private Long id;
+    private String name;
+    private int age;
+
+    public User(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
