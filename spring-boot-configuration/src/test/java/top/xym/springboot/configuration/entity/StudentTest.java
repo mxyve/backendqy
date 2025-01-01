f0d3fe8c0fd34b9a0f664ba16a55b9b933379bac
缪心雨 <1286280961@qq.com>
2025-01-01 20:01:02 +0800

    较复杂的配置示例

---
+++ spring-boot-configuration/src/test/java/top/xym/springboot/configuration/entity/StudentTest.java
@@ -0,0 +1,22 @@
+package top.xym.springboot.configuration.entity;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class StudentTest {
+    @Resource
+    private Dog dog;
+
+    @Resource
+    private Student student;
+
+    @Test
+    void  testStudent() {
+        log.info(String.valueOf(dog));
+        log.info(String.valueOf(student));
+    }
+}
