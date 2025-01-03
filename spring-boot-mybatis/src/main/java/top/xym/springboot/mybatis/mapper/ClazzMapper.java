7f3d2e5cb42f27e8c33f86b4244ee2da02590d8f
缪心雨 <1286280961@qq.com>
2025-01-03 16:22:30 +0800

    2.4.1 查询班级信息

--- spring-boot-mybatis/src/main/java/top/xym/springboot/mybatis/mapper/ClazzMapper.java
+++ spring-boot-mybatis/src/main/java/top/xym/springboot/mybatis/mapper/ClazzMapper.java
@@ -10,4 +10,12 @@ public interface ClazzMapper {
      * @return 班级信息
      */
     Clazz getClazzOneToMany(int clazzId);
+
+    /**
+     * 根据班级id查询班级信息
+     *
+     * @param clazzId 班级id
+     * @return 班级对象（班级自身信息、班级教师信息、班级所有学生信息）
+     */
+    Clazz getClazz(int clazzId);
 }
--- spring-boot-mybatis/src/test/java/top/xym/springboot/mybatis/mapper/ClazzMapperTest.java
+++ spring-boot-mybatis/src/test/java/top/xym/springboot/mybatis/mapper/ClazzMapperTest.java
@@ -21,4 +21,13 @@ class ClazzMapperTest {
         // .forEach() 是 Java 8 引入的 Stream API 中的一个方法，用于对集合中的每个元素执行一个操作。
         clazz.getStudents().forEach(student -> log.info(String.valueOf(student)));
     }
+
+    @Test
+    void getClazz() {
+        Clazz clazz = clazzMapper.getClazz(1);
+        log.info("班级名称：{}", clazz.getClazzName());
+        log.info("任课老师：{}", clazz.getTeacher().getTeacherName());
+        log.info("班级学生信息如下：");
+        clazz.getStudents().forEach(student -> log.info("{},{}", student.getStudentName(), student.getHometown()));
+    }
 }
