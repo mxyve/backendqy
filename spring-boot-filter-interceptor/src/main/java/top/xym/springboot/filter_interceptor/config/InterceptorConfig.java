121b8ee10eb59e039b9825a6f484172ee567a0dc
缪心雨 <1286280961@qq.com>
2025-01-07 16:46:03 +0800

    2.5 响应内容处理拦截器

--- spring-boot-filter-interceptor/src/main/java/top/xym/springboot/filter_interceptor/config/InterceptorConfig.java
+++ spring-boot-filter-interceptor/src/main/java/top/xym/springboot/filter_interceptor/config/InterceptorConfig.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import top.xym.springboot.filter_interceptor.interceptor.AuthInterceptor;
-import top.xym.springboot.filter_interceptor.interceptor.CORSInterceptor;
-import top.xym.springboot.filter_interceptor.interceptor.LoggingInterceptor;
-import top.xym.springboot.filter_interceptor.interceptor.RequestModificationInterceptor;
+import top.xym.springboot.filter_interceptor.interceptor.*;
 
 @Configuration
 @AllArgsConstructor
@@ -15,12 +12,14 @@ public class InterceptorConfig implements WebMvcConfigurer {
 //    private final LoggingInterceptor loggingInterceptor;
 //    private final AuthInterceptor authInterceptor;
 //    private final CORSInterceptor corsInterceptor;
-    private final RequestModificationInterceptor requestModificationInterceptor;
+//    private final RequestModificationInterceptor requestModificationInterceptor;
+    private final ResponseModificationInterceptor responseModificationInterceptor;
 
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(loggingInterceptor).addPathPatterns("/**");
 //        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
 //        registry.addInterceptor((corsInterceptor)).addPathPatterns("/**");
-        registry.addInterceptor(requestModificationInterceptor).addPathPatterns("/**");
+//        registry.addInterceptor(requestModificationInterceptor).addPathPatterns("/**");
+    registry.addInterceptor(responseModificationInterceptor).addPathPatterns("/**");
     }
 }
