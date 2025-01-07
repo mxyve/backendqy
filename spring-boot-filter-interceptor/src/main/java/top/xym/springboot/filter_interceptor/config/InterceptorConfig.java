3b4a83bbb8987762a482bb2a4a0faea254b0db1b
缪心雨 <1286280961@qq.com>
2025-01-07 16:04:50 +0800

    2.3 跨域处理拦截器

--- spring-boot-filter-interceptor/src/main/java/top/xym/springboot/filter_interceptor/config/InterceptorConfig.java
+++ spring-boot-filter-interceptor/src/main/java/top/xym/springboot/filter_interceptor/config/InterceptorConfig.java
@@ -5,16 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import top.xym.springboot.filter_interceptor.interceptor.AuthInterceptor;
+import top.xym.springboot.filter_interceptor.interceptor.CORSInterceptor;
 import top.xym.springboot.filter_interceptor.interceptor.LoggingInterceptor;
 
 @Configuration
 @AllArgsConstructor
 public class InterceptorConfig implements WebMvcConfigurer {
 //    private final LoggingInterceptor loggingInterceptor;
-    private final AuthInterceptor authInterceptor;
+//    private final AuthInterceptor authInterceptor;
+    private final CORSInterceptor corsInterceptor;
 
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(loggingInterceptor).addPathPatterns("/**");
-        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+//        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+        registry.addInterceptor((corsInterceptor)).addPathPatterns("/**");
     }
 }
